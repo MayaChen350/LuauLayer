18ea0f4936b4fc0d4238d2f31105da2a4d16220a
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2025-06-03 03:26:39 +0200

    Remove unused method override from LuauThread class

--- src/main/kotlin/evo/lualayer/wrapper/LuauThread.kt
+++ src/main/kotlin/evo/lualayer/wrapper/LuauThread.kt
@@ -4,10 +4,6 @@ import net.hollowcube.luau.LuaState
 
 class LuauThread(private val parent: LuaState) : State(lua = parent.newThread()) {
 
-    override fun sandbox() {
-        lua.sandbox()
-    }
-
     override fun close() {
         parent.pop(1)
     }
