37388905e568cd3d08e6c710cbaa2f57db9bf5de
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2025-06-03 02:49:51 +0200

    Update LuauConfig

--- src/main/kotlin/evo/LGlobal.kt
+++ src/main/kotlin/evo/LGlobal.kt
@@ -4,15 +4,8 @@ import evo.State
 import net.hollowcube.luau.compiler.LuauCompiler
 
 class LuauConfig(
-    val paths: Set<String>,
+    val paths: Set<String> = setOf("run/scripts"),
     val compiler: LuauCompiler = Compilers.DEBUG,
 ) {
     val state = State(config = this)
-
-    companion object {
-        val DEFAULT = LuauConfig(
-            paths = setOf("run/scripts"),
-            compiler = Compilers.DEBUG,
-        )
-    }
 }
