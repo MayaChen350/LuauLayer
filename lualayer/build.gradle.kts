cb47db5041a514e811d3e7cb4605282dbd57c981
Maya <145165822+MayaChen350@users.noreply.github.com>
2025-09-07 20:08:50 -0400

    Add build support for Macos+Linux

--- lualayer/build.gradle.kts
+++ lualayer/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.os.OperatingSystem
+
 plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.ksp)
@@ -13,8 +15,16 @@ dependencies {
     implementation(libs.okio)
     implementation(libs.bundles.prettylog)
     implementation(libs.bundles.kotlinx)
-    implementation(files("libs/luau-natives-windows-x64-${libs.versions.luau.get()}.jar"))
-
+    implementation(
+        when (OperatingSystem.current()) {
+            OperatingSystem.WINDOWS -> "dev.hollowcube:luau-natives-windows-x64:${libs.versions.luau.get()}"
+            OperatingSystem.LINUX -> "dev.hollowcube:luau-natives-linux-x64:${libs.versions.luau.get()}"
+            OperatingSystem.MAC_OS -> "dev.hollowcube:luau-natives-macos-arm64:${libs.versions.luau.get()}"
+            else -> {
+                throw GradleException("Operating system is not currently supported for luau.")
+            }
+        }
+    )
     api(project(":annotations"))
 }
 
